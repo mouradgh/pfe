0215ad17ba3011644053e53f7de1ae5d97cdeaf7
mouradgh <mouradelghissassi1992@gmail.com>
2016-02-07 19:35:36 +0100

    feat : add a google map view
    google map view with a pointer to localisation
    
    closes #12

---
+++ SpeedTest/src/main/java/com/speedtest/MapView.java
@@ -0,0 +1,38 @@
+package com.speedtest;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapView extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_view);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        SharedPreferences sp = getSharedPreferences("session", Context.MODE_PRIVATE);
+        LatLng speed = new LatLng(Double.parseDouble(sp.getString("mapX", "")), Double.parseDouble(sp.getString("mapY", "")));
+        float down = sp.getFloat("avgDown", 0);
+        float up = sp.getFloat("avgUp", 0);
+        mMap.addMarker(new MarkerOptions().position(speed).title("Downloading: "+down+"\nUploading: "+up));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(speed));
+    }
+}
