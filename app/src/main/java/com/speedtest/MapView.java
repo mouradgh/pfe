b51b61a6fc48bd38431b296f9a6c4fa73b340ae5
MehdiBak <just.bikili@gmail.com>
2016-02-11 14:38:33 +0100

    map

--- app/src/main/java/com/speedtest/MapView.java
+++ app/src/main/java/com/speedtest/MapView.java
@@ -1,30 +1,42 @@
 package com.speedtest;
 
+/**
+ * Created by Admin on 10/02/2016.
+ */
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+    import android.content.Context;
+    import android.content.SharedPreferences;
+    import android.os.Bundle;
+    import android.support.v4.app.FragmentActivity;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+    import com.google.android.gms.maps.CameraUpdateFactory;
+    import com.google.android.gms.maps.GoogleMap;
+    import com.google.android.gms.maps.OnMapReadyCallback;
+    import com.google.android.gms.maps.SupportMapFragment;
+    import com.google.android.gms.maps.model.LatLng;
+    import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MapView extends FragmentActivity implements OnMapReadyCallback {
 
-public class MapView extends AppCompatActivity  implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_view);
-
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        SharedPreferences sp = getSharedPreferences("session", Context.MODE_PRIVATE);
+        LatLng speed = new LatLng(Double.parseDouble(sp.getString("mapX", "")), Double.parseDouble(sp.getString("mapY", "")));
+        float down = sp.getFloat("avgDown", 0);
+        float up = sp.getFloat("avgUp", 0);
+        mMap.addMarker(new MarkerOptions().position(speed).title("Downloading: "+down+"\nUploading: "+up));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(speed));
     }
 }
