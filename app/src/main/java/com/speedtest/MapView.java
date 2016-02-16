8c24a905a8731f021df2f7c3db5a3788699be43f
MehdiBak <just.bikili@gmail.com>
2016-02-16 13:45:37 +0100

    changing markers on the map to colored transparent circles

--- app/src/main/java/com/speedtest/MapView.java
+++ app/src/main/java/com/speedtest/MapView.java
@@ -4,6 +4,7 @@ package com.speedtest;
  * Created by Admin on 10/02/2016.
  */
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -24,6 +25,10 @@ import java.util.List;
 
 public class MapView extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    int BLUE = 0x500000FF; //transparency fixed to 50%
+    int RED=0x50FF0000;
+    int YELLOW=0x50FFFF00;
+    int GREEN=0x5000FF00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +41,16 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        float Color;
+        int color;
         List<DataModel> dataModelList = FileUtils.ParseDataFile(this, FileUtils.GetRootPath(this) + FileUtils.CHECK_SPEED_RESULT_FILE);
-        LatLng loc;
+        LatLng loc=null;
         // Adding marker for every location in the file
         if (dataModelList != null && dataModelList.size() > 0) {
             for (DataModel dataModel : dataModelList) {
                 loc = dataModel.getLocation();
-                Color = getColorSpeed(dataModel.getDownloadSpeed());
-                mMap.addMarker(new MarkerOptions().position(loc).icon(BitmapDescriptorFactory.defaultMarker(Color)));
+                color = getColorSpeed(dataModel.getDownloadSpeed());
+               // mMap.addMarker(new MarkerOptions().position(loc).icon(BitmapDescriptorFactory.defaultMarker(color)));
+                mMap.addCircle(new CircleOptions().center(loc).fillColor(color).radius(30.0).strokeColor(Color.TRANSPARENT));
             }
         }
         // setting the geolocation to true
@@ -53,16 +59,16 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback {
     }
 
     // getting the color according to the download speed
-    private float getColorSpeed(float downloadSpeed) {
-        float Color;
+    private int getColorSpeed(float downloadSpeed) {
+        int Color;
         if(downloadSpeed < 300.0)
-            Color = BitmapDescriptorFactory.HUE_RED;
+            Color = RED;
         else if(downloadSpeed < 800.0)
-            Color = BitmapDescriptorFactory.HUE_YELLOW;
+            Color =YELLOW;
         else if(downloadSpeed < 1200.0)
-            Color = BitmapDescriptorFactory.HUE_BLUE;
+            Color = BLUE;
         else
-            Color = BitmapDescriptorFactory.HUE_GREEN;
+            Color = GREEN;
         return Color;
     }
 }
