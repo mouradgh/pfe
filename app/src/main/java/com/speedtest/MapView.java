2a52f20c2d51003f9c729671a7e27ef5683bcb05
MehdiBak <just.bikili@gmail.com>
2016-02-17 13:13:22 +0100

    Adding RadioButtons on the map to choose between different types of networks

--- app/src/main/java/com/speedtest/MapView.java
+++ app/src/main/java/com/speedtest/MapView.java
@@ -7,8 +7,10 @@ package com.speedtest;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,6 +29,7 @@ import java.util.List;
 
 public class MapView extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
+    List<DataModel> dataModelList;
     int BLUE = 0x500000FF; //transparency fixed to 50%
     int RED=0x50FF0000;
     int YELLOW=0x50FFFF00;
@@ -36,6 +39,7 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_view);
+        dataModelList = FileUtils.ParseDataFile(this, FileUtils.GetRootPath(this) + FileUtils.CHECK_SPEED_RESULT_FILE);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -43,24 +47,39 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        int color;
-        List<DataModel> dataModelList = FileUtils.ParseDataFile(this, FileUtils.GetRootPath(this) + FileUtils.CHECK_SPEED_RESULT_FILE);
-        LatLng loc=null;
-        // Adding cirlces for every location in the file
+
+        // Adding circles for every location in the file
         if (dataModelList != null && dataModelList.size() > 0) {
-            for (DataModel dataModel : dataModelList) {
-                loc = dataModel.getLocation();
-                color = getColorSpeed(dataModel.getDownloadSpeed());
-               // mMap.addMarker(new MarkerOptions().position(loc).icon(BitmapDescriptorFactory.defaultMarker(color)));
-                mMap.addCircle(new CircleOptions().center(loc).fillColor(color).radius(30.0).strokeColor(Color.TRANSPARENT));
-            }
+            AddCirclesToMAP(dataModelList,"");
         }
         // setting the geolocation to true
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setCompassEnabled(true);
-
     }
 
+    private void AddCirclesToMAP(List<DataModel> dataModelList, String type){
+        int color;
+        LatLng loc;
+        if (type.isEmpty()){
+            mMap.clear();
+            for (DataModel dataModel : dataModelList) {
+                loc = dataModel.getLocation();
+                color = getColorSpeed(dataModel.getDownloadSpeed());
+                mMap.addCircle(new CircleOptions().center(loc).fillColor(color).radius(30.0).strokeColor(Color.TRANSPARENT));
+            }
+        }
+        else{
+            mMap.clear();
+            for (DataModel dataModel : dataModelList) {
+                if (dataModel.getConnectionType().contains(type)) {
+                    loc = dataModel.getLocation();
+                    color = getColorSpeed(dataModel.getDownloadSpeed());
+                    mMap.addCircle(new CircleOptions().center(loc).fillColor(color).radius(30.0).strokeColor(Color.TRANSPARENT));
+                }
+            }
+        }
+
+    }
     // getting the color according to the download speed
     private int getColorSpeed(float downloadSpeed) {
         int Color;
@@ -73,5 +92,32 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback {
         else
             Color = GREEN;
         return Color;
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioButton:
+                if (checked) {
+                    mMap.clear();
+                    AddCirclesToMAP(dataModelList, "3G");
+                }
+                    break;
+            case R.id.radioButton2:
+                if (checked) {
+                    mMap.clear();
+                    AddCirclesToMAP(dataModelList, "4G");
+                }
+                    break;
+            case R.id.radioButton3:
+                if (checked) {
+                    mMap.clear();
+                    AddCirclesToMAP(dataModelList, "WIFI");
+                }
+                break;
+        }
     }
 }
